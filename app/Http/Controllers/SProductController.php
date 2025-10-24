81331a540665d6c8a75ed3c5b49bb987050b2b6c
FarrelRayhan <farrel.putra292@gmail.com>
2025-10-24 13:31:57 +0700

    don

---
+++ app/Http/Controllers/SProductController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SProductController extends Controller
+{
+    private $products = [
+        ['id' => 1, 'name' => 'Tenda Dome 2 Orang', 'price' => 250000],
+        ['id' => 2, 'name' => 'Tenda Dome 4 Orang', 'price' => 150000],
+        ['id' => 3, 'name' => 'Carrier 60L', 'price' => 400000],
+    ];
+
+    public function index(Request $request)
+    {
+        $keyword = strtolower((string) $request->get('search', ''));
+
+        $filtered = array_values(array_filter($this->products, function($product) use ($keyword) {
+            if ($keyword === '') {
+                return true;
+            }
+            return str_contains(strtolower($product['name']), $keyword);
+        }));
+
+        return view('products', [
+            'products' => $filtered,
+            'keyword' => $keyword,
+        ]);
+    }
+}
---
+++ resources/views/Product.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Halaman Penjualan</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+    <!-- Navbar -->
+    <div class="navbar">
+        <div class="brand">Campify</div>
+        <form action="{{ route('search-products') }}" method="GET">
+            <input type="text" name="search" placeholder="Cari produk..." value="{{ $keyword }}">
+            <button type="submit">Cari</button>
+        </form>
+    </div>
+
+    <div class="container">
+        <h2>Daftar Produk</h2>
+
+        @if(count($products) > 0)
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nama Produk</th>
+                        <th>Harga</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($products as $p)
+                        <tr>
+                            <td>{{ $p['id'] }}</td>
+                            <td>{{ $p['name'] }}</td>
+                            <td>Rp {{ number_format($p['price'], 0, ',', '.') }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>Tidak ada produk ditemukan.</p>
+        @endif
+    </div>
+
+</body>
+</html>
